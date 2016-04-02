4d5d2623cf56df6f0ffdca7c92cca99dcb99d854
S <league@iMac.attlocal.net>
2016-04-01 18:29:29 -0700

    April Fool's Day
    Pokemon Game Start

--- src/GameWindows.java
+++ src/GameWindows.java
@@ -5,36 +5,42 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class GameWindows implements ActionListener, KeyListener, MouseListener, MouseMotionListener
-{
+public class GameWindows implements ActionListener, KeyListener, MouseListener, MouseMotionListener {
 	JFrame frame;
 	JPanelWindows panel;
 	private int width, height;
 	KeyListener key;
 	MouseListener mouse;
 	MouseMotionListener mouseL;
-	
-	
-	public GameWindows()
-	{
+	BufferedImage background1;
+	JButton button, buttonClicked;
+
+	int count;
+	private final int RED = -3920896;
+	private final int GREEN = -10450126;
+	private final int BLUE = -11981403;
+	private final int YELLOW = -1456;
+	private final int WHITE=-1052432 ;
+
+	public GameWindows() {
 		width = 800;
 		height = 600;
-	}
-	
-	
-	public static void main(String[] args)
-	{
-		GameWindows w = new GameWindows();
-		w.createUI();
-		
+		count = 0;
 	}
 
-	public void createUI()
-	{
+	public static void main(String[] args) {
+		GameWindows w = new GameWindows();
+		w.createUI();
+
+	}
+
+	public void createUI() {
 		frame = new JFrame();
 		frame.setVisible(true);
 		panel = new JPanelWindows();
@@ -44,96 +50,122 @@ public class GameWindows implements ActionListener, KeyListener, MouseListener, 
 		panel.addMouseListener(this);
 		panel.addMouseMotionListener(this);
 		panel.addKeyListener(this);
-		
+
+		try {
+			background1 = ImageIO.read(this.getClass().getResourceAsStream("flyingTurtleScreen1.png"));
+
+		} catch (Exception e) {
+			System.out.println("Problem");
+		}
+
 	}
 
-	public int getWidth()
-	{
+	public int getWidth() {
 		return 800;
-	
+
 	}
-	
-	public int getHeight()
-	{
+
+	public int getHeight() {
 		return 600;
 	}
-
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Mouse Dragged");
 	}
-
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Mouse Moved");
+		
 	}
-
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Mouse Clicked");
+		
+		int mouseX = e.getX();
+		int mouseY = e.getY();
+		if(panel.getCount() == 1) {
+			int mouseColor = background1.getRGB(mouseX, mouseY);
+				if(mouseColor == RED)
+					panel.setCount(2);
+				if(mouseColor == GREEN)
+					panel.setCount(3);
+				if(mouseColor == BLUE)
+					panel.setCount(4);
+				if(mouseColor == YELLOW)
+					panel.setCount(5);
+				
+		}
+		if(panel.getCount() == 2){
+			int mouseColor = background1.getRGB(mouseX, mouseY);
+		if(mouseColor == RED)
+			panel.setCount(2);
+		if(mouseColor == GREEN)
+			panel.setCount(3);
+		if(mouseColor == BLUE)
+			panel.setCount(4);
+		if(mouseColor == YELLOW)
+			panel.setCount(5);
+		if(mouseColor == WHITE)
+			panel.setCount(1);
+		}
+		else{
+			int mouseColor = background1.getRGB(mouseX, mouseY);
+			if(mouseColor == WHITE)
+				panel.setCount(1);
+			}
 	}
-
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Mouse Pressed");
-	}
+		if (panel.getCount() == 0)
+			panel.setCount(1);
 
+	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Mouse Released");
+	
 	}
-
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Mouse Entered");
-	}
 
+	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Mouse Exited");
+	
 	}
-
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Key Typed");
+	
 	}
-
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Key Pressed");
+		
 	}
-
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Key Released");
+	
 	}
-
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-	
-	}
-	
-}
 
+	}
+
+}
--- src/JPanelWindows.java
+++ src/JPanelWindows.java
@@ -7,50 +7,76 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-
-public class JPanelWindows extends JPanel implements ActionListener
-{
+public class JPanelWindows extends JPanel implements ActionListener {
 	Timer t;
+	int count;
 	GameObject player;
-	TurtleObject turtle, backgroundPage, pikachu;
-	BufferedImage pika;
-	BufferedImage image, background;
+	TurtleObject turtle, backgroundPage, backgroundPage2, bp3, bp4, bp5, bp6, bp7;
+	BufferedImage background1, background2, backgroundTurtleAttack, backgroundTurtleRun, backgroundTurtleStuff, backgroundTurtlePoke;
 	GameWindows w;
+
+	public JPanelWindows() {
+		
+		t = new Timer(1000 , this);
+
+	    
+			try {
+				background1 = ImageIO.read(this.getClass().getResourceAsStream("BackgroundPoke.png"));
+				background2 = ImageIO.read(this.getClass().getResourceAsStream("flyingTurtleScreen1.png"));
+				backgroundTurtleAttack =  ImageIO.read(this.getClass().getResourceAsStream("PokemonTurtleScreen2.png"));
+				backgroundTurtlePoke =  ImageIO.read(this.getClass().getResourceAsStream("PokemonTurtleScreenPokemon.png"));
+				backgroundTurtleRun =  ImageIO.read(this.getClass().getResourceAsStream("PokemonTurtleScreenRun.png"));
+				backgroundTurtleStuff =  ImageIO.read(this.getClass().getResourceAsStream("PokemonTurtleScreenStuff.png"));
+
+			} 
+			catch (Exception e) {
+				//System.out.println("Problem");
+			}
 	
-	public JPanelWindows()
-	{	
-		
-		
-		t = new Timer(1000/60, this);
+		backgroundPage = new TurtleObject(1, 1, 800, 600, background1);
+		backgroundPage2 = new TurtleObject(1, 1, 800, 600, background2);
+		bp3 = new TurtleObject(1, 1, 800, 600, backgroundTurtleAttack);
+		bp4 = new TurtleObject(1, 1, 800, 600, backgroundTurtlePoke);
+		bp5 = new TurtleObject(1, 1, 800, 600, backgroundTurtleRun);
+		bp6 = new TurtleObject(1, 1, 800, 600, backgroundTurtleStuff);
 		t.start();
-		try
-		{
-			image = ImageIO.read(this.getClass().getResourceAsStream("tutl.png"));
-			background = ImageIO.read(this.getClass().getResourceAsStream("FlyingTurtleB.png"));
-			pika = ImageIO.read(this.getClass().getResourceAsStream("pika.png"));
-		}
-		catch(Exception e)
-		{
-			System.out.println("Problem");
-		}    
-		
-		turtle = new TurtleObject(500,60,200,200,image);
-		backgroundPage = new TurtleObject(1,1,800,600, background);
-		pikachu = new TurtleObject(100,200,74*3,94*3,pika);
-		
 	}
 	
-	public void paintComponent(Graphics g)
-	{
+
+	public void paintComponent(Graphics g) {
+		if(count ==0){
 		backgroundPage.draw(g);
-		turtle.draw(g);
-		pikachu.draw(g);
-	
+		}
+		if(count == 1)
+			backgroundPage2.draw(g);
+		if(count == 2)
+			bp3.draw(g);
+		if(count == 3)
+			bp4.draw(g);
+		if(count == 4)
+			bp5.draw(g);
+		if(count == 5)
+			bp6.draw(g);
+		
+		
 	}
-	@Override
-	public void actionPerformed(ActionEvent e)
+	
+	public void setCount(int s)
 	{
+		this.count = s;
+	}
+	
+	public int getCount()
+	{
+		return count;
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
 		repaint();
+		
+		
 	}
 
 }
